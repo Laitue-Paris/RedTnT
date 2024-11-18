cb95164539b35e375a3826ab7e556e5bf48932f2
xavier bahegne <xavierbahegne@gmail.com>
2024-11-18 16:17:27 +0100

    add rentals table and user_id to clothes

--- db/migrate/20241118142709_add_user_references_to_clothes.rb
+++ db/migrate/20241118142709_add_user_references_to_clothes.rb
@@ -1,5 +1,5 @@
 class AddUserReferencesToClothes < ActiveRecord::Migration[7.1]
   def change
-    add_foreign_key :clothes, :user
+    add_reference :clothes, :user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_142459) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_142709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_142459) do
     t.boolean "available", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_clothes_on_user_id"
   end
 
   create_table "rentals", force: :cascade do |t|
