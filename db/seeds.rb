bbb409ba07569efd7a4379b275eff10e86420e23
xavier bahegne <xavierbahegne@gmail.com>
2024-11-19 11:27:02 +0100

    add 4 examples of T-shirt

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts "Cleaning the database..."
+Clothe.destroy_all
+
+puts "Creating 4 Clothes for a rental website of Red T-shirt"
+
+Clothe.create(
+  name: "Red T-shirt",
+  color: "Red",
+  price: [19, 29, 39, 49, 59].sample,
+  size: ["S", "M", "L", "XL"].sample,
+  category: ["T-shirt", "Sweatshirt"].sample,
+  description: "This is a red T-shirt",
+)
+
+Clothe.create(
+  name: "RedT",
+  color: "Red Tomato",
+  price: [19, 29, 39, 49, 59].sample,
+  size: ["S", "M", "L", "XL"].sample,
+  category: ["T-shirt", "Sweatshirt"].sample,
+  description: "This is a red T-shirt",
+)
+
+Clothe.create(
+  name: "Re(a)d",
+  color: "Strawberry Red",
+  price: [19, 29, 39, 49, 59].sample,
+  size: ["S", "M", "L", "XL"].sample,
+  category: ["T-shirt", "Sweatshirt"].sample,
+  description: "This is a red T-shirt",
+)
+
+Clothe.create(
+  name: "Red T",
+  color: "Red",
+  price: [19, 29, 39, 49, 59].sample,
+  size: ["S", "M", "L", "XL"].sample,
+  category: ["T-shirt", "Sweatshirt"].sample,
+  description: "This is a red T-shirt",
+)
+
+puts "Finished creating #{Clothe.count} Clothes "
