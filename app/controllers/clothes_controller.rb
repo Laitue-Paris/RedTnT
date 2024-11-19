23c95b0bcba1c51717cea72054d8b78a4d317497
najoua lamtioui <najoua-lamtioui@hotmail.fr>
2024-11-19 16:00:26 +0100

    clothes controller

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -1,10 +1,24 @@
 class ClothesController < ApplicationController
   def index
+    @clothes = Clothe.all
   end
 
   def show
+    @clothe = Clothe.find(params[:id])
   end
 
   def create
+    @clothe = Clothe.new(clothe_params)
+    if @clothe.save
+      redirect_to clothe_path(@clothe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def clothe_params
+    params.require(:clothe).permit(:name)
   end
 end
