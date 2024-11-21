2a121c1891f41b9805c565c5870f3dfdcf073fed
xavier bahegne <xavierbahegne@gmail.com>
2024-11-21 17:04:52 +0100

    add maths for revenus

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,8 @@ class DashboardController < ApplicationController
     @clothe = Clothe.new
     @rentals = Rental.all
     @rentals = @rentals.where("start_date <= ? AND end_date >= ? AND user_id = ?", Date.today, Date.today, current_user.id)
+    @total_rented = @user.rentals.map do |rental|
+      rental.clothe[:price]
+    end
   end
 end
