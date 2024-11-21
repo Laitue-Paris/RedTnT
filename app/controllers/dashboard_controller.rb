df082b274f4f6a3bf79d3a66317e0575da0ac8bb
xavier bahegne <xavierbahegne@gmail.com>
2024-11-21 16:51:25 +0100

    add arithmetic logic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   def index
     @user = current_user
     @clothe = Clothe.new
+    @rentals = Rental.all
+    @rentals = @rentals.where("start_date <= ? AND end_date >= ? AND user_id = ?", Date.today, Date.today, current_user.id)
   end
 end
